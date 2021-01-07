f7ce7d97e0e86fc0d6b4f52dad595a0240b1e523
mk1999 <keerthanamahendiren@gmail.com>
2021-01-07 18:02:03 +0530

    Update NavDrawerActivity.kt

--- TodoListApp/app/src/main/java/com/example/todolistapp/NavDrawerActivity.kt
+++ TodoListApp/app/src/main/java/com/example/todolistapp/NavDrawerActivity.kt
@@ -16,35 +16,70 @@ import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 
-class NavDrawerActivity : AppCompatActivity() {
+class NavDrawerActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
+    val navView: NavigationView = findViewById(R.id.nav_view)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.nav_drawer)
+        navView.setNavigationItemSelectedListener(this)
+        navigateActions()
+    }
+
+    fun navigateActions(){
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
-
+        val actionbar: ActionBar? = supportActionBar
+        actionbar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+        }
+        var drawerToggle:ActionBarDrawerToggle = ActionBarDrawerToggle(this,drawer_layout,R.string.open,R.string.close)
+        drawerToggle.isDrawerIndicatorEnabled = true
+        drawer_layout.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
 
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-        val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
-            ), drawerLayout
-        )
+        appBarConfiguration = AppBarConfiguration(setOf(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
     }
 
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.nav_drawer, menu)
+        menuInflater.inflate(R.menu.activity_main_drawer, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_settings -> return true
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.nav_home -> {
+                supportFragmentManager.beginTransaction().replace(R.id.nav_host_fragment,HomeFragment()).commit()
+            }
+            R.id.nav_gallery->{
+                supportFragmentManager.beginTransaction().replace(R.id.nav_host_fragment,GalleryFragment()).commit()
+            }
+            R.id.nav_slideshow -> {
+                supportFragmentManager.beginTransaction().replace(R.id.nav_host_fragment,SlideshowFragment()).commit()
+            }
+        }
+        drawer_layout.closeDrawer(GravityCompat.START)
         return true
     }
 
